8c808139ff98b40e5c8a78dfda2339642ddabc87
Carlos Gallardo <carlitosgallardo009@gmail.com>
2022-04-20 19:21:43 -0500

    Add project files.

---
+++ Lab05/Lab05/ImageResourceExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+namespace Lab05
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    internal class ImageResourceExtension : IMarkupExtension
+    {
+    }
+}
